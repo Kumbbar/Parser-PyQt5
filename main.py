3c445690b038cf292be50e5127d551e8d04089d7
Kumbbar <damirnasibullin8@gmail.com>
2022-05-24 22:57:55 +0500

    0.1 interface fixed

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-from urllib.error import URLError
-import urllib3
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from bs4 import BeautifulSoup
 import requests
 import sys
-import urllib
+
 
 class MainWindow(QMainWindow):  # главное окно
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle("Parser")
         self.setWindowIcon(QIcon('icons/icon-planet.svg'))
-        self.high = 800
-        self.weight = 800
+        self.high = 700
+        self.weight = 200
         self.setup_ui()
 
     def setup_ui(self):
@@ -22,29 +20,48 @@ class MainWindow(QMainWindow):  # главное окно
         self.centralWidget()
         self.setFixedSize(self.high, self.weight)
         self.textbox = QLineEdit(self)
-        self.textbox.move(20, 30)
+        self.textbox.move(30, 30)
         self.textbox.resize(500, 40)
-        self.textbox.setText('Input text')
+        self.textbox.setPlaceholderText('Input url:')
         self.textbox2 = QLineEdit(self)
-        self.textbox2.move(100, 200)
-        self.textbox2.resize(400, 40)
-        self.label = QLabel('<h1>Enter url<h1>', self)
-        self.label.resize(600, 50)
-        self.label.move(0, 55)
+        self.textbox2.move(30, 80)
+        self.textbox2.resize(500, 40)
+        self.textbox2.setPlaceholderText('Input tag:')
         self.button = QPushButton('Parse', self)
-        self.button.resize(100, 50)
+        self.button.resize(100, 200)
         self.button.clicked.connect(self.parse_click)
-        self.label.setAlignment(Qt.AlignCenter)
         self.button.setStyleSheet("QPushButton"
                                   "{"
-                                  "background-color : lightblue;"
+                                  "background-color : #e0dbd7;"
                                   "border-radius : 5px"
                                   "}"
                                   "QPushButton::pressed"
                                   "{"
-                                  "background-color : yellow;"
-                                  "}")
-        self.button.move(250, 300)
+                                  "background-color : white;"
+                                  "}"
+                                  "{"
+                                  "text-color : white;"
+                                  "}"
+                                  )
+        self.button.move(600, 0)
+
+        self.settings_button = QPushButton('Save settings', self)
+        self.settings_button.resize(100, 40)
+        self.settings_button.clicked.connect(self.open_settings_dialog)
+        self.settings_button.setStyleSheet("QPushButton"
+                                           "{"
+                                           "background-color : #e0dbd7;"
+                                           "border-radius : 5px"
+                                           "}"
+                                           "QPushButton::pressed"
+                                           "{"
+                                           "background-color : white;"
+                                           "}"
+                                           "{"
+                                           "text-color : white;"
+                                           "}"
+                                           )
+        self.settings_button.move(430, 130)
 
     def parse_click(self):
         try:
@@ -57,6 +74,9 @@ class MainWindow(QMainWindow):  # главное окно
                     file.write(request_html)
         except ValueError:
             return 0
+
+    def open_settings_dialog(self):
+        pass
 
 
 if __name__ == "__main__":
